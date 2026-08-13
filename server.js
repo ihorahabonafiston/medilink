58e96a7cfa037e6859f687228ce4b9bf76129182
Internee Account <internee@cimbugarama.local>
2026-08-13 09:38:07 +0300

    Use environment port for deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 9000;
+const PORT = process.env.PORT || 6000;
 
 // ======================================================
 // MIDDLEWARE
